94b93637ef7a2b575a601db194570b10dcb19622
Barchard <lb185070@ncr.com>
2017-02-20 21:46:49 -0500

    Added all 4 characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,32 @@
 (function(){//***********************Begin Global Variables***********************//
+var obi-wan = {
+	id: "obi-wan",
+	name: "Obi-Wan Kenobi",
+	image: "obi-wan.jpg",
+	health: "120",
+}
+
+var obi-wan = {
+	id: "luke",
+	name: "Luke Skywalker",
+	image: "Luke",
+	health: "100",
+}
+
+var obi-wan = {
+	id: "sidious",
+	name: "Darth Sidious",
+	image: "sidious.jpg",
+	health: "150",
+}
+
+var obi-wan = {
+	id: "maul",
+	name: "Darth Maul",
+	image: "maul.jpg",
+	health: "180",
+}
+
 
 
 
